657897193925ef066c98e38b9ee4f6de0599b44b
Mahmud Emre BAŞPEHLİVAN <93977436+Mahmud-Baspehlivan@users.noreply.github.com>
2024-02-11 11:33:05 +0300

    Movie List

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+
+import App from './components/App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+createRoot(document.getElementById('root')).render(<App />);
